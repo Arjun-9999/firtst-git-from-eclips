5222658a460bd85f159e0149c532d16cc90e0d36
Arjun-9999 <112554028+Arjun-9999@users.noreply.github.com>
2022-08-31 18:33:26 +0530

    Add files via upload

---
+++ this.is.artifact/src/main/java/TestingMaven.java
@@ -0,0 +1,13 @@
+
+public class TestingMaven {
+
+	void first() {
+		System.out.println("first methoddd   ");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first maven project ");
+	}
+
+}
